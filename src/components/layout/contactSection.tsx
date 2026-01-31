698cf5640ed571b710b3912992cb593b5a966c13
Victor Nogueira <victorjose.nogueirasantos@hotmail.com>
2026-01-31 03:34:38 -0300

    css

--- src/components/layout/contactSection.tsx
+++ src/components/layout/contactSection.tsx
@@ -1,8 +1,68 @@
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub,faWhatsapp,faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+
+// ... resto do seu código
 function ContactSection() {
+
+    const contactLinks = {
+        emal: {
+            buttonName: "Email",
+            textLabel:"victorvalim1@gmail.com",
+            link: '',
+            icon: faEnvelope
+        },
+        github: {
+            buttonName: "Github",
+            textLabel:"github/VictorJoseNogueira",
+            link: 'https://github.com/VictorJoseNogueira',
+            icon: faGithub
+        },
+        linkedin: {
+            buttonName: "Linkedin",
+            textLabel:"linkedin/in/victor-nogueira-dev",
+            link: 'https://linkedin.com/in/victor-nogueira-dev',
+            icon: faLinkedin
+        },
+        watsapp: {
+            buttonName: "Watsapp",
+            textLabel:"wa.me/+55 (11)9 4907-9807",
+            link: 'https://wa.me/5511949079807',
+            icon: faWhatsapp
+        },
+        
+    }
+
     return (
         <section id='contact-section' className="contact-section">
         <h1>Contato</h1>
-        <span>Email:</span> <a href="mailto:victorvalim1@gmail.com">victorvalim1@gmail.com</a>
+        <div className="letter-container">
+        { Object.entries(contactLinks).map(([key, value]) => (
+        <React.Fragment key={key}>
+            {value.link === "" ? (
+                <span className="letter-button-links"><strong>
+                <FontAwesomeIcon icon={value.icon} />
+            </strong> {value.textLabel}</span>    
+        ):(
+            <a className="letter-button-links" target="_blank" rel="noopener noreferrer"
+            href={value.link}><strong>
+                <FontAwesomeIcon icon={value.icon} />
+            </strong> {value.textLabel}</a>
+        )
+    }
+            </React.Fragment>
+    ))}
+           
+        <div className="cursive-container">
+            <span className="cursive-text">
+                Vamos construir algo juntos? Sinta-se à vontade para me chamar em qualquer uma das redes acima.
+            </span>
+        </div>
+    </div>
+        
+
         </section>
     );
 }
